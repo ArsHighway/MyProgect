fb4f312b06ea1f244ea39778fa517bf9c5f391a4
ArsHighway <rool18list@gmail.com>
2025-10-02 12:43:47 +0300

    1) Запись выражения ввиде строки. 2) Добавление ф-ции которая проверяет наличие букв в колькуляторе и повторяющихся знаков операции.

--- start.go
+++ start.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
 
 func main() {
-	fmt.Println("Hello")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Введите выражение: ")
+	mathematical_operation, _ := reader.ReadString('\n')
+	mathematical_operation = strings.TrimSpace(mathematical_operation)
+	fmt.Println(isTure(mathematical_operation))
+	fmt.Println(mathematical_operation)
+}
+
+func isTure(str string) bool {
+	newStr := strings.Replace(str, " ", "", -1)
+	for i, n := range newStr {
+		if !(unicode.IsDigit(n)) && !(unicode.IsSymbol(n)) {
+			return false
+		} else if i != len(newStr)-1 {
+			if unicode.IsSymbol(rune(newStr[i])) && unicode.IsSymbol(rune(newStr[i+1])) {
+				return false
+			} else if unicode.IsSymbol(rune(newStr[i])) && unicode.IsSymbol(rune(newStr[len(newStr)-1])) {
+				return false
+			}
+		} else if i == len(newStr)-1 && unicode.IsSymbol(rune(newStr[len(newStr)-1])) {
+			return false
+		}
+	}
+	return true
 }
